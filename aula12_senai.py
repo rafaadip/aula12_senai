2e3986aedfef8cc3bf66df9594602cf4f3b87536
rafaadip <39415423+rafaadip@users.noreply.github.com>
2023-10-06 16:52:08 -0300

    Update aula12_senai.py

--- aula12_senai.py
+++ aula12_senai.py
@@ -54,3 +54,81 @@ conjunto1 = [2,6,4,7]
 conjunto2 = [8,56,67,87]
 for conjunto1 in conjunto2:
     print(conjunto2)
+
+### METODO DIFFERENCE
+# conjunto1={'value0','Value'}
+# conjunto2={'value1','Value2'}
+# print(conjunto1.difference(conjunto2))
+# print(f'No conjunto 1 tem {conjunto2.difference(conjunto1)}, esses não tem no conjunto2')
+
+# ### METODO UNIAO
+# print('união')
+# uniao = conjunto1.union(conjunto2)
+# print(f'Os 2 conjuntos unidos: {uniao}\n')
+
+# ### TRANSFORMANDO EM LISTA
+# print('lista')
+# lista = list(conjunto1)
+# print(lista, '\n')
+
+# ### TRANSFORMANDO LISTA EM CONJUNTO
+# print('conjunto')
+# lista4=[1,2,3,4,5,88]
+# conjunto=set(lista4)
+# print(conjunto)
+
+### EXERCICIOS
+
+### 1: **Transforme uma lista em um conjunto**
+# # Criar uma lista
+# lista = [1, 2, 3, 4, 5]
+# # Transformar a lista em um conjunto
+# conjunto1 = set(lista)
+# # Imprimir o conjunto
+# print('conjunto1:', conjunto1)
+
+
+### 2: **Una estes 2 conjuntos:**
+# zone = {1, 2, 3, 4}
+# kwia = {3, 4, 5, 6}
+# print('união')
+# uniao = zone.union(kwia)
+# print(uniao)
+
+### 3: **Verifique a diferença entre os conjuntos:**
+# g1 = {12,3569,1,5,7}
+# g2 = {12,3569,2,5,8}
+# print(g1.difference(g2))
+# print(f'No conjunto 1 tem {g1.difference(g2)}, esses não tem no conjunto2')
+
+### 4: Crie um conjunto chamado "frutas" com as seguintes frutas: maçã, banana, laranja, pêra e abacaxi. Em seguida, imprima o número de elementos no conjunto.
+# frutas = {'maçã','banana','laranja','pêra','abacaxi'}
+# tamanho= len(frutas)
+# print(tamanho)
+
+### 5: Crie dois conjuntos, "conjunto1" e "conjunto2",com alguns números inteiros. Imprima a união desses dois conjuntos
+# conjunto1 = {1,2,3,4,5,6,7,8,9}
+# conjunto2 = {11,22,33,44,55,66,77,88,99}
+# uniao = conjunto1.union(conjunto2)
+# print(uniao)
+
+### 6: Dado o conjunto "cores" com cores diferentes, remova a cor "vermelho" do conjunto.
+# cores = {'Azul','Vermelho','Preto','Rosa'}
+# cores.remove('Vermelho')
+# print(cores)
+
+### 7: Crie um conjunto chamado "numeros" com os números de 1 a 10. Em seguida, crie um novo conjunto chamado "pares" contendo apenas os números pares do conjunto "numeros".
+# numeros = {1,2,3,4,5,6,7,8,9,10}  
+# pares = list(filter(lambda x: x % 2 == 0, numeros))
+# print(pares)
+
+### 8: Verifique se o conjunto "alunos_aprovados" contém todos os alunos do conjunto "todos_alunos". Os conjuntos devem ser definidos com nomes apropriados.
+# alunos_aprovados = {'aluno2','aluno5','aluno3'}
+# todos_alunos = {'aluno1','aluno2','aluno3','aluno4','aluno5','aluno6'}
+# print(todos_alunos.difference(alunos_aprovados))
+
+
+
+
+
+
